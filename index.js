01f6d1d25f2f9b635dde6e5b23b20319d9699425
Daniel Heller <danielkheller@gmail.com>
2023-09-07 11:37:12 -0700

    revert to basic events feed

--- index.js
+++ index.js
@@ -23,88 +23,19 @@ const port = process.env.PORT || 80;
 		var eventsjson = responsejson.actions[0].returnValue;
 //		fs.writeFileSync('data/events.json', JSON.stringify(eventsjson));
 		console.log( eventsjson.length + " events found.");
-
-//		ss = location.search.substring(1);
-//		if(ss) {
-//			if(ss.indexOf('add=')!=-1) {
-//				el = $(".add li a.service");
-//				loadcustomize();
-//			}
-
-
-//		http.createServer(function (req, res) {
-//			if( eventsjson && eventsjson.length ) {
-//				res.writeHead(200, {'Content-Type': 'text/json'});
-//				res.end( JSON.stringify(eventsjson) );
-//			} else {
-//				res.writeHead(200, {'Content-Type': 'text/html'});
-//				res.end( 'false' );
-//			}
-//		}).listen( port );
-//		console.log( "Displayed JSON.");
-
-		const affinities = ["Affinity: Asian Pacific","Affinity: Black","Affinity: Latino/x","Affinity: LGBTQ"];
-		await page.waitForSelector('select[name="selectedtopicInterests"]');
-		var mergedeventsjson = eventsjson;
-		page
-			.on('console', message => {
-				const msgtxt = `${message.text()}`;
-				if( 0 == msgtxt.indexOf('result: q') ) {
-					affinityresults = msgtxt.substring( msgtxt.indexOf('[') );
-					affinityresultsjson = JSON.parse( affinityresults );
-//					console.log( affinityresultsjson.length + " events found.");
-//					console.log( apaaresultsjson );
-
-					mergedeventsjson = [
-						...affinityresultsjson,
-						...mergedeventsjson
-					];
-//					console.log( "Total events: " + mergedeventsjson.length );
-				}
-			});
-
-
-		var affinityresults = '';
-		var affinityresultsjson = [];
-		affinities.forEach( function(aff,index) {
-			var delay = 1000 + ( index * 2000 );
-			setTimeout( function() {
-				// select dropdown item for Asian Pacific Alumni Association
-				page.select('select', aff);
-//				console.log( 'selected '+aff );
-
-				page.waitForSelector('button.slds-button.slds-button_brand.w-full');
-				page.click('button.slds-button.slds-button_brand.w-full');
-
-				//setTimeout( function() {
-				//	page.screenshot({
-				//	  path: 'screenshotafterbutton.jpg'
-				//	});
-				//}, 5000 );
-
-//				console.log( 'clicked Search' );
-				// get a console log beginning with "result:"
-			}, delay );
-		});
-
-		setTimeout(function() {
-
-			http.createServer(function (req, res) {
-				if( apaajson ) {
-					res.writeHead(200, {'Content-Type': 'text/json'});
-					res.end( JSON.stringify( mergedeventsjson ) );
-				} else {
-					res.writeHead(200, {'Content-Type': 'text/html'});
-					res.end( 'false' );
-				}
-			}).listen( port );
-			console.log( mergedeventsjson.length + " events found.");
-//			console.log( mergedeventsjson );
-			browser.close();
-		}, 10000 );
-
+		http.createServer(function (req, res) {
+			if( eventsjson && eventsjson.length ) {
+				res.writeHead(200, {'Content-Type': 'text/json'});
+				res.end( JSON.stringify(eventsjson) );
+			} else {
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end( 'false' );
+			}
+		}).listen( port );
+		console.log( "Displayed JSON.");
 	}  catch(err) {
 		console.log(err);
 	}
+	await browser.close();
 
 })();
